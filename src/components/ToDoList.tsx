b3b1215f5252dde6a9e19b3df6aa6d67c8d70269
Hyun Seok Park <luckseok1@gmail.com>
2022-12-19 23:26:36 +0900

    feat: Recoil Selectors를 이용한 filter 기능 구현

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,24 +1,34 @@
 import React from "react";
-import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atoms";
 import AddToDo from "./AddToDo";
 import ToDo from "./ToDo";
 
 export default function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  console.log(category);
 
-  console.log(toDos);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setCategory(value);
+  };
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <AddToDo />
-      <ul>
-        {toDos.map((todo) => (
-          <ToDo key={todo.id} {...todo} />
-        ))}
-      </ul>
+      {toDos?.map((todo) => (
+        <ToDo key={todo.id} {...todo} />
+      ))}
     </div>
   );
 }
