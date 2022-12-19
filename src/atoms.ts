ade9f2db7e35d796e60128699604a08c55d50fac
Hyun Seok Park <luckseok1@gmail.com>
2022-12-20 01:40:35 +0900

    refactor: LocalStorage -> recoil-persist 라이브러리 사용하여 리팩토링

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,4 +1,5 @@
 import { atom, selector } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 export enum Categories {
   TODO = "TO_DO",
@@ -17,24 +18,29 @@ export const categoryState = atom<Categories>({
   default: Categories.TODO,
 });
 
-const localStorageEffect =
-  (key: string) =>
-  ({ setSelf, onSet }: any) => {
-    const savedToDos = localStorage.getItem(key);
-    if (savedToDos !== null) {
-      setSelf(JSON.parse(savedToDos));
-    }
-    onSet((toDos: any, _: any, isReSet: boolean) => {
-      isReSet
-        ? localStorage.removeItem(key)
-        : localStorage.setItem(key, JSON.stringify(toDos));
-    });
-  };
+// const localStorageEffect =
+//   (key: string) =>
+//   ({ setSelf, onSet }: any) => {
+//     const savedToDos = localStorage.getItem(key);
+//     if (savedToDos !== null) {
+//       setSelf(JSON.parse(savedToDos));
+//     }
+//     onSet((toDos: any, _: any, isReSet: boolean) => {
+//       isReSet
+//         ? localStorage.removeItem(key)
+//         : localStorage.setItem(key, JSON.stringify(toDos));
+//     });
+//   };
+
+const { persistAtom } = recoilPersist({
+  key: "todoLocal",
+  storage: localStorage,
+});
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
-  effects: [localStorageEffect("toDos")],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const toDoSelector = selector({
