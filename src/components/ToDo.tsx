ac5f634eacf0e89ac1d7f7fa1ded319e1bae6827
Hyun Seok Park <luckseok1@gmail.com>
2022-12-19 11:40:41 +0900

    feat: Category Update

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,28 @@
 import React from "react";
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
 export default function ToDo({ text, id, category }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (newCategory: IToDo["category"]) => {
+    setToDos((prevToDos) =>
+      prevToDos.map((toDo) =>
+        toDo.id === id ? { ...toDo, category: newCategory } : toDo
+      )
+    );
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
